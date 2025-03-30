2a5f14b7b5bcec9e869d97231463577051e82227
serdarera11 <kralserdarok@gmail.com>
2025-03-31 00:24:42 +0300

    Add files via upload

---
+++ database.py
@@ -0,0 +1,29 @@
+import time
+
+def simulate_hack():
+    print("Bağlantı kuruluyor...")
+    time.sleep(1)
+    print("Veritabanı bağlantısı sağlandı...")
+    time.sleep(1)
+    print("Veriler çekiliyor...")
+    time.sleep(1)
+    print("Veritabanı .txt dosyasına kaydedildi: database.txt")
+    time.sleep(1)
+    print("\n\nVeri tabanı kaydedildi.")
+
+def main():
+    print("Veritabanını çekmek istediğiniz sitenin adını girin:")
+    site = input("Site Adı: ")
+    print(f"{site} veritabanı çekiliyor...")
+    simulate_hack()
+    
+    with open("database.txt", "w") as file:
+        file.write(f"Site: {site}\n")
+        file.write("=" * 30 + "\n")
+        file.write("Dosya başarı ile oluşturuldu!\n")
+
+    print("database.txt oluşturuldu! Dosyayı açıp bakabilirsiniz.")
+
+
+if __name__ == "__main__":
+    main()
